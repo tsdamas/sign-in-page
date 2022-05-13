977b3d6e5d594fe926a5eeb8e07336fe57f06e74
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-05-12 19:40:28 -0700

    Added function and design
    - Created a function to handle log in;
    - Started style;

--- sign-in-project/src/App.js
+++ sign-in-project/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.production.min';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -8,27 +8,73 @@ function App() {
     //state indicates if the value is submitted or not
     const [isSubmitted, setIsSubmitted] = useState(false); 
 
+    //user login info object 
+    const database = [
+      {
+        username:'user1',
+        password:'pass1'
+      },
+      {
+        username:'user2',
+        password:'pass2'
+      },
+      {
+        username:'user3',
+        password:'pass3'
+      }
+    ];
+
+    const errors = {
+      uname:'invalid username',
+      pass: 'invalid password'
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault(); 
+
+      var {uname, pass} = document.forms[0];
+
+      //Find user data
+      const userData = database.find((user) => user.username === uname.value); 
+
+      //Compare user info with input value
+      if (userData) {
+        if (userData.password !== pass.value) {
+          //incorrect password 
+          setErrorMessages({name:'pass', message: errors.pass});
+        } else {
+          setIsSubmitted(true);
+        } 
+      } else {
+        //incorrect usernamme
+          setErrorMessages({name:'uname', message: errors.uname}); 
+        }
+    }
+
+
     //generate JSX code for error message
     const handleErrorMessages = (name) => 
     name === errorMessages.name && (
-      <div className="error"> {errorMessages.message}</div>
+      <div className="error">{errorMessages.message}</div>
     ); 
+
 
     // JSX code for sign in form
     const renderForm = (
       <div className='form'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='user-input-container'>
             <label>Username</label>
-            <input type='text' name='uname' required />
-            {handleErrorMessages("uname")}
+            <input type='text' className='uname mb-5' required />
+            {handleErrorMessages('uname')}
           </div>
           <div className='password-input-container'>
-            <input type='password' name='class' required/>
-            {handleErrorMessages("pass")}
+            <label>Password</label>
+            <input type='password' className='pass mb-3' required/>
+            {handleErrorMessages('pass')}
           </div>
           <div className='button-container'>
-            <input type='submit'/>
+            <input type='submit' className='button'/>
           </div>
         </form>
       </div>
@@ -39,7 +85,8 @@ function App() {
       <header className='App-header'>
       </header>
       <div className='sign-in-form'>
-        <div className='title'>Sign in to your account</div>
+        <div className='title'>Log in to your account</div>
+        <h6>Not a member?  <a href='/'>Sign up</a></h6>
         {isSubmitted ? <div>User is sucessfully logged in</div> : renderForm}
       </div>
     </div>
